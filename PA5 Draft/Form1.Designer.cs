5ffdea37c7e2e3d11e10029c823c6c8afdb67a10
jcher050 <35442284+jcher050@users.noreply.github.com>
2021-01-21 21:03:17 -0500

    Add project files.

---
+++ PA5 Draft/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace PA5_Draft
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Field = new System.Windows.Forms.PictureBox();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Field)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Field
+            // 
+            this.Field.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Field.Location = new System.Drawing.Point(0, 41);
+            this.Field.Name = "Field";
+            this.Field.Size = new System.Drawing.Size(803, 623);
+            this.Field.TabIndex = 0;
+            this.Field.TabStop = false;
+            this.Field.Click += new System.EventHandler(this.PauseResume);
+            this.Field.Paint += new System.Windows.Forms.PaintEventHandler(this.Field_Paint);
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Enabled = true;
+            this.mainTimer.Interval = 12;
+            this.mainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(183, 0);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(123, 29);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(654, 0);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(0);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(149, 30);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(306, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select Apple Amount to start";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 1);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Snake Speed";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 705);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Field);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(821, 752);
+            this.MinimumSize = new System.Drawing.Size(821, 752);
+            this.Name = "MainForm";
+            this.Text = "Snakes";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Snakes_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Field)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Field;
+        private System.Windows.Forms.Timer mainTimer;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PA5 Draft/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PA5_Draft
+{
+    public partial class MainForm : Form
+    {
+        private int Step;//speed of the snake moving 
+        private readonly SnakeGame Game;
+        private int NumberOfApples = 2000; //Number of apple for when the game start
+        private SoundPlayer Applechew; //sounds for the game
+        private SoundPlayer Wallhit;
+        private SoundPlayer Snakehit;
+        public MainForm()
+        {
+            InitializeComponent();
+            //Beginapple = new ChooseNumAppletobegin();
+            //Beginapple.ShowDialog();
+            //NumberOfApples = Beginapple.SelectedNum;
+            //Beginapple.Dispose();
+            Step = 1;
+
+                    Game = new SnakeGame(new System.Drawing.Point((Field.Width - 20) / 2, Field.Height / 2), 40, NumberOfApples, Field.Size);
+                    Field.Image = new Bitmap(Field.Width, Field.Height);
+                    Game.EatAndGrow += Game_EatAndGrow;
+                    Game.HitWallAndLose += Game_HitWallAndLose;
+                    Game.HitSnakeAndLose += Game_HitSnakeAndLose;
+                    Applechew = new SoundPlayer(@"sounds\eatapple.wav");
+                    Wallhit = new SoundPlayer(@"sounds\tap.wav");
+                    Snakehit = new SoundPlayer(@"sounds\Ha.wav");
+                
+                
+            
+        }
+
+        private void Game_HitWallAndLose()
+        {
+            Wallhit.Play();
+            mainTimer.Stop();
+            Field.Refresh();
+
+          
+
+        }
+        private void Game_HitSnakeAndLose()
+        {
+            Snakehit.Play();
+            mainTimer.Stop();
+            Field.Refresh();
+        }
+
+        private void Game_EatAndGrow()
+        {
+            Applechew.Play();
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            //if (Beginapple.ShowDialog() == DialogResult.OK)
+            //{
+
+            Game.Move(Step);
+           Field.Invalidate();
+           // }
+        }
+
+        private void Field_Paint(object sender, PaintEventArgs e)
+        {
+            Random r = new Random();
+            int one = r.Next(0, 255);
+            int two = r.Next(0, 255);
+            int three = r.Next(0, 255);
+            Pen PenForObstacles = new Pen(Color.FromArgb(40,40,40),2);
+            Pen PenForSnake = new Pen(Color.FromArgb(100, 100, 100), 2);
+            Brush BackGroundBrush = new SolidBrush(Color.FromArgb(150, 250, 150));
+            Brush AppleBrush = new SolidBrush(Color.FromArgb(one, two, three));
+
+
+
+            using (Graphics g = Graphics.FromImage(Field.Image))
+            {
+                g.FillRectangle(BackGroundBrush,new Rectangle(0,0,Field.Width,Field.Height));
+                foreach (Point Apple in Game.Apples)
+                    g.FillEllipse(AppleBrush, new Rectangle(Apple.X - SnakeGame.AppleSize / 2, Apple.Y - SnakeGame.AppleSize / 2,
+                        SnakeGame.AppleSize, SnakeGame.AppleSize));
+                foreach (LineSeg Obstacle in Game.Obstacles)
+                    g.DrawLine(PenForObstacles, new System.Drawing.Point(Obstacle.Start.X, Obstacle.Start.Y)
+                        , new System.Drawing.Point(Obstacle.End.X, Obstacle.End.Y));
+                foreach (LineSeg Body in Game.SnakeBody)
+                    g.DrawLine(PenForSnake, new System.Drawing.Point((int)Body.Start.X, (int)Body.Start.Y)
+                        , new System.Drawing.Point((int)Body.End.X, (int)Body.End.Y));
+            }
+        }
+
+
+
+        private void Snakes_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    Game.Move(Step, Direction.UP);
+                    break;
+                case Keys.Down:
+                    Game.Move(Step, Direction.DOWN);
+                    break;
+                case Keys.Left:
+                    Game.Move(Step, Direction.LEFT);
+                    break;
+                case Keys.Right:
+                    Game.Move(Step, Direction.RIGHT);
+                    break;
+            }
+        }
+
+        private void PauseResume(object sender, EventArgs e)
+        {
+            mainTimer.Enabled = !mainTimer.Enabled;
+        }
+    }
+}
